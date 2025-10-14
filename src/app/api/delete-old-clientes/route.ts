bfd0fdf55394b58dc913ee76153e74483da14b58
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-10-14 13:18:34 -0300

    Remove Whats and add corn job

---
+++ src/app/api/delete-old-clientes/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/mongodb";
+import { ClienteModel } from "@/models/Cliente";
+import { DateTime } from "luxon";
+
+export async function GET(req: NextRequest) {
+  await connectToDatabase();
+
+  const doisDiasAtras = DateTime.now()
+    .setZone("America/Sao_Paulo")
+    .minus({ days: 2 })
+    .startOf("day");
+
+  const result = await ClienteModel.deleteMany({
+    date: { $lte: doisDiasAtras.toFormat("yyyy-MM-dd") },
+  });
+
+  return NextResponse.json(
+    { message: `Clientes excluídos: ${result.deletedCount}` },
+    { status: 200 }
+  );
+}
