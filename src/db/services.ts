dcf7401741f9f2265f403c118191f44a2c9b72a7
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2026-05-17 18:55:25 -0300

    Update Duration

--- src/db/services.ts
+++ src/db/services.ts
@@ -1,6 +1,6 @@
 export const services = [
-  { name: "Corte", price: 40, duration: 40 },
-  { name: "Barba", price: 40, duration: 30 },
-  { name: "Acabamento (Pezinho)", price: 20, duration: 20 },
-  { name: "Penteado", price: 40, duration: 30 },
+  { name: "Corte", price: 40, duration: 60 },
+  { name: "Barba", price: 40, duration: 60 },
+  { name: "Acabamento (Pezinho)", price: 20, duration: 30 },
+  { name: "Penteado", price: 40, duration: 60 },
 ];
