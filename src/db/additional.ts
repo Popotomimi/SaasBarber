ad1bced5dcc376ce8b4e06a4fbb71e49e4715879
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2026-05-17 20:25:18 -0300

    Change Duration on Additional.ts

--- src/db/additional.ts
+++ src/db/additional.ts
@@ -1,9 +1,9 @@
 export const additionalServices = [
-  { name: "Sobrancelha", price: 10, duration: 5 },
-  { name: "Bigode", price: 10, duration: 5 },
-  { name: "Limpeza de pele", price: 30, duration: 10 },
-  { name: "Barba", price: 30, duration: 25 },
-  { name: "Hidratação", price: 20, duration: 15 },
-  { name: "Depilação nasal", price: 25, duration: 10 },
-  { name: "Penteado selado", price: 30, duration: 25 },
+  { name: "Sobrancelha", price: 10, duration: 0 },
+  { name: "Bigode", price: 10, duration: 0 },
+  { name: "Limpeza de pele", price: 30, duration: 0 },
+  { name: "Barba", price: 30, duration: 0 },
+  { name: "Hidratação", price: 20, duration: 0 },
+  { name: "Depilação nasal", price: 25, duration: 0 },
+  { name: "Penteado selado", price: 30, duration: 0 },
 ];
